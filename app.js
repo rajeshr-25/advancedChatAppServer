659c3fc0b57d96a6fc1a5154c48e428d0c68db5a
rajeshr-25 <rajeshwaranr12345@gmail.com>
2026-03-03 00:32:15 +0530

    fix: error fixed

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ import authRoutes from "./auth/auth.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://advanced-chat-app-client.vercel.app",
+    "http://localhost:5500"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Public routes
